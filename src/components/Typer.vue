ef056cc19df693c76e23e735f9f7fef5128e3aeb
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2021-12-18 22:32:06 +0100

    Typing highlighting & some styling

--- src/components/Typer.vue
+++ src/components/Typer.vue
@@ -1,37 +1,60 @@
 <template>
-    <div class="typer-container">
-        <div class="">
-            <h1>This is the text that you should type out</h1>
-            <p>{{text}}</p>
+    <div class="container typer-container">
+        <h1 class="text-center row h-10">SpeedTyper</h1>
+        <div class="row h-20 text-container">
+            <p><span v-for="word in words" :key="word.id">
+                <span v-bind:class="{highlight: word.text === this.currentWord}">{{word.text}}</span>
+                {{" "}}
+            </span>
+            </p>
         </div>
-        <div class="input-container">
-            <h1>This is the input</h1>
-            <form v-on:keydown.space="nextWord">
-                <input type="text" v-model="input" >
+        <div class="row h-40 input-container">
+            <form v-on:keydown.space="nextWord" v-on:submit.prevent>
+                <input placeholder="..." type="text" class="input-field" v-model="input" autofocus>
             </form>
-            <p>{{result}}</p>
-            <p>{{currentWord}}</p>
-            <p>{{counter}} </p>
         </div>
     </div>
 </template>
 
 <script>
+import highlightWords from 'highlight-words';
+
 export default {
     name: 'Typer',
     components: {},
     computed: {
+        text: function() {
+            return this.paragraph
+        },
         result: function () {
-            return this.input
+            return this.input   
+        },
+        words: function () {
+            let splitWords = this.text.split(" ") 
+            let words = []
+            for(let i = 0 ; i <= splitWords.length; i++) {
+                let word = {
+                    id : i,
+                    text : splitWords[i]
+                }
+                words[i] = word
+            }
+            return words
         },
         currentWord: function() {
-            const words = this.text.split(" ") 
-            return words[this.counter]
+            return this.words[this.counter].text
+        },
+        chunks: function() {
+            return highlightWords({
+                text: this.paragraph,
+                query: this.input,
+                matchExactly: true,
+            })
         }
     },
     data: function(){
         return {
-            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ipsam sequi, quis minus architecto temporibus nihil officiis quisquam nisi laudantium doloremque voluptatibus laborum est, aspernatur ratione quibusdam. Asperiores, natus corporis!",
+            paragraph: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ipsam sequi, quis minus architecto temporibus nihil officiis quisquam nisi laudantium doloremque voluptatibus laborum est, aspernatur ratione quibusdam. Asperiores, natus corporis!",
             input: "",
             counter: 0,
         }
@@ -43,7 +66,7 @@ export default {
                 this.counter++
                 this.input = ""
             }
-        }
+        },
     }
 }
 </script>
