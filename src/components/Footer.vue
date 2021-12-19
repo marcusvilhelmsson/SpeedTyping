51b71eae10819110af764d931399ae94efa73755
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2021-12-19 15:10:46 +0100

    Github link in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container footer-container">
+    <div class="row text-center">
+        <p class="col-12 mt-5">
+            <font-awesome-icon
+                class="icon"
+                size="lg"
+                :icon="['fab', 'github']"
+                @click="goTo('github')"  />
+        </p>
+    </div>
     <div class="row">
-        <p class="col-6 mx-auto text-center">Test</p>
-        <p class="col-6"><font-awesome-icon
-                        size="lg"
-                        :icon="['fab', 'github']"
-                        @click="goTo('github')"  /></p>
+        <p class="col-12 mx-auto text-center mt-2 mb-5">Licensed by @Marcus Vilhelmsson, Creative Commons <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="Creative Commons-licens" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/80x15.png" /></a> </p>
+        
     </div>
   </div>
 </template>
