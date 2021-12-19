b94cae5e6b56b178639bbef30e9bd5af2ec551b5
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2021-12-19 13:57:25 +0100

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="app-container">
     <Typer/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Typer from './components/Typer.vue'
-
+import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
-    Typer
+    Typer,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container footer-container">
+    <div class="row">
+        <p class="col-6 mx-auto text-center">Test</p>
+        <p class="col-6"><font-awesome-icon
+                        size="lg"
+                        :icon="['fab', 'github']"
+                        @click="goTo('github')"  /></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import information from '../mixins/information'
+export default {
+  name: 'Footer',
+  components: {},
+  mixins: [information],
+  methods: {
+        goTo: function(link){
+          const information = this.getContactInformation()
+          switch(link){
+            case 'github':
+                window.open(information.githubLink)
+                break;
+          }
+      }
+  }
+}
+</script>
