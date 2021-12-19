e8f1181508417575386869ceb781fb40bc610de1
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2021-12-19 13:57:10 +0100

    Added fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import "../public/scss/_style.scss";
 
@@ -11,5 +12,6 @@ library.add(fas)
 library.add(fab)
 
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
 
